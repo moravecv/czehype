00342decc089edc3b5dc681caf343bde99c28487
moravecv <moravec.vojtech@gmail.com>
2017-11-24 13:46:28 +0100

    renamehypefile function

---
+++ renamehypefile.R
@@ -0,0 +1,30 @@
+#' Renames columns of input file according to a table of names
+#' in such manner that HYPE recognizes
+#' 
+#' @param in_file input file to be renamed
+#' @param name_table table of names
+#' @param col_old string name of column in table of names with old name
+#' @param col_new string name of column in table of names with replacement
+#'
+#' @return renamed input file
+#' @export
+#'
+#' @examples
+#' 
+#' 
+#' 
+
+renamehypefile <- function(in_file, name_table, col_old, col_new){
+  library(data.table)
+  in_file <- data.table(in_file) 
+  name_table <- data.table(name_table)
+  for (i in colnames(in_file)[2:ncol(in_file)]){ # for each column name
+    a <- name_table[get(col_old) == i, get(col_new)] # find new name in name table 
+    b <- which(colnames(in_file) == i) # find a position of old column name in input file
+    colnames(in_file)[b] <- a # replace the old column name with a new one
+  }
+  return(in_file) # return renamed input file
+}
+
+
+
