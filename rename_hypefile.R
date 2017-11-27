9672657124c276be205a2dfeee3c8802648042ed
moravecv <moravec.vojtech@gmail.com>
2017-11-27 16:17:05 +0100

    variable names

--- rename_hypefile.R
+++ rename_hypefile.R
@@ -27,9 +27,9 @@ renamehypefile <- function(in_file, name_table, col_old, col_new){
   in_file <- data.table(in_file) 
   name_table <- data.table(name_table)
   for (i in colnames(in_file)[2:ncol(in_file)]){ # for each column name
-    a <- name_table[get(col_old) == i, get(col_new)] # find new name in name table 
-    b <- which(colnames(in_file) == i) # find a position of old column name in input file
-    colnames(in_file)[b] <- a # replace the old column name with a new one
+    new_name <- name_table[get(col_old) == i, get(col_new)] # find new name in name table 
+    position <- which(colnames(in_file) == i) # find a position of old column name in input file
+    colnames(in_file)[position] <- new_name # replace the old column name with a new one
   }
   return(in_file) # return renamed input file
 }
