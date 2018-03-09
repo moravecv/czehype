19ad206fdd1edd4b91801c990efcb9cb8a0feda9
moravecv <moravec.vojtech@gmail.com>
2018-03-09 10:20:13 +0100

    progbar, comments

--- lu_change.R
+++ lu_change.R
@@ -13,15 +13,25 @@
 #' gdnew = lu_change(from = 2, to = 5, fraction = 0.3, subid = c(1,9,15), gd = gd, gc = gc)
 
 lu_change <- function(from, to, fraction, subid, gd, gc){
-  library(HYPEtools)
+  # desc: Moves fraction of specified landuse to another landuse class 
+  # (for simulating landuse change)
+  # arg from: - which landuse will be fraction taken 
+  # arg to: - which landuse will be fraction moved
+  # arg fraction: of landuse to be moved (0 - 1)
+  # arg subid: vector of subids where landuse will be moved
+  # arg gd: data.frame of GeoData
+  # arg gc: data.frame of GeoClass
+  # return: data.frame GeoData with moved fractions 
   
-  #n <- length(subid)
-  #pb <- txtProgressBar(min = 0, max = n, style = 3)
-  for (j in subid){
-    print(j)
+  library(HYPEtools)
+  nofrac <- c() # empty ector for subids with no fraction to move
+  n <- length(subid)
+  pb <- txtProgressBar(min = 0, max = n, style = 3)
+  for (j in 1:n){
+    #print(j) # debug line
     gdcols.slc <- which(toupper(substr(names(gd), 1, 3)) == "SLC") # indexes of SLC columns
-    #slc <- gd[which(gd$SUBID == subid[j]), gdcols.slc]
-    slc <- gd[which(gd$SUBID == j), gdcols.slc] # SCL columns for specified subid
+    slc <- gd[which(gd$SUBID == subid[j]), gdcols.slc]
+    #slc <- gd[which(gd$SUBID == j), gdcols.slc] # SCL columns for specified subid
     original <- slc # save original of SLC row for specified subid
     
     if(fraction == 1) {
@@ -35,7 +45,7 @@ lu_change <- function(from, to, fraction, subid, gd, gc){
       val_change <- fraction * sum(slc[,gc[which(gc[,2] == from),1]])
       # total fraction to be moved from separate SLC's
       if (val_change == 0) {
-        message("Warning: Subid ", j, " has 0 fraction to move")
+        nofrac <- append(nofrac,j) # add subid which has no fraction to move in vector
       } else {
         fixing <- slc[,gc[which(gc[,2] == from),1]] # SLC's of landuse to be moved
         pointers <- which(fixing != 0) # indexes of values to be modified
@@ -76,7 +86,7 @@ lu_change <- function(from, to, fraction, subid, gd, gc){
       # indexes of SLC's with the same depth as the one which was moved
       depth.fit2 <- depth.fit[depth.fit %in% text.fit]
       # intersect of the texture and depth
-      if (length(depth.fit2 == 0)){
+      if (length(depth.fit2) == 0){
         # if there was no suitable combination found, spread the fraction among other SLC's with 
         # the same structure i.e. among various depth
         slc[depth.fit] <- slc[depth.fit] + (unlist(diff) / length(slc[depth.fit]))
@@ -86,10 +96,11 @@ lu_change <- function(from, to, fraction, subid, gd, gc){
         # recalculate value that is increased by fraction deducted from SLC 
       }
     }
-    gd[which(gd$SUBID == j), gdcols.slc] <- slc # write vector of fixed values in the gd dataframe
-    #gd[which(gd$SUBID == subid[j]), gdcols.slc] <- slc
-    #setTxtProgressBar(pb, j)
+    #gd[which(gd$SUBID == j), gdcols.slc] <- slc # write vector of fixed values in the gd dataframe
+    gd[which(gd$SUBID == subid[j]), gdcols.slc] <- slc
+    setTxtProgressBar(pb, j)
   }
-  #close(pb)
+  close(pb)
+  print(nofrac) # print out vector of subids with no fraction to move
   return(gd)
 }
